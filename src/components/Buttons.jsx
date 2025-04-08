00d7b367de17d0cba651c66039e8a81dd324c899
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-04-08 18:16:52 +0200

    feat: add card and button event

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,23 @@
+// importare useState
+import { useState } from "react"
+
+// importare la card
+import Card from "./Card"
+
+
 // funzione bottone
 export default function Button({ title }) {
+
+    const [click, setClick] = useState(false)
+
+    const toggle = () => {
+        setClick(prev => !prev);
+    }
+
     return (
         <div>
-            <button>{title}</button>
+            <button onClick={toggle}>{title}</button>
+            {click && <Card />}
         </div>
     )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+// funzione della card
+export default function Card() {
+    return (
+        <div>
+            <p>sono la card</p>
+        </div>
+
+    )
+}
